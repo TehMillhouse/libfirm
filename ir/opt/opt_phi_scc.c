25f6989b75eb8dfa9ee962947965c7d23d190ff4
Max Wagner <max@trollbu.de>
2016-03-27 21:43:38 +0200

    Working recursion into induced subgraphs (I think)
    Thing is, I don't have a graph to test this with yet. It looks good (?)
    though.

--- ir/opt/opt_phi_scc.c
+++ ir/opt/opt_phi_scc.c
@@ -14,32 +14,17 @@
  */
 
 #include "irdump.h"
-#include "adt/pdeq.h"
 #include "adt/list.h"
 #include "array.h"
 #include "debug.h"
-#include "panic.h"
-#include "hashptr.h"
 #include "ircons.h"
-#include "irdom.h"
 #include "iredges_t.h"
-#include "irflag_t.h"
-#include "irgmod.h"
 #include "irgwalk.h"
-#include "irhooks.h"
+#include "irnodehashmap.h"
 #include "irnodeset.h"
-#include "irloop_t.h"
-#include "irop_t.h"
-#include "iroptimize.h"
-#include "irouts.h"
 #include "irtools.h"
-#include "obst.h"
-#include "set.h"
-#include "tv.h"
 #include "util.h"
 
-/** The debug handle. */
-DEBUG_ONLY(static firm_dbg_module_t *dbg;)
 
 /** We use (yet another implementation of) Tarjan's algorithm to find SCCs, which implicitly gives them
  * to us in reverse topological order, which is incidentally exactly the ordering we need.
@@ -59,10 +44,7 @@ DEBUG_ONLY(static firm_dbg_module_t *dbg;)
  *
  * this means we need to save arbitrarily many SCCs, and we need a quick way to exclude nodes from the searched
  * node set.
- * SCCs need to be iterable, so I'll need some sort of dynamic list in there
  */
-
-/** SCCs are represented as linked lists of nodes and stored in a linked list themselves. */
 
 typedef struct scc_node {
     list_head    link;
@@ -74,14 +56,15 @@ typedef struct scc_env {
     ir_node        **stack;
     size_t           stack_top;
     unsigned         next_index;
-    list_head        sccs;        /**< doubly-linked list of sccs */
+    list_head        sccs;             /**< doubly-linked list of sccs */
+    ir_nodehashmap_t replacement_map;  /**< map from node to their replacement */
 } scc_env_t;
 
 typedef struct scc_irn_info {
     bool     in_stack;          /**< Marks whether node is on the stack. */
     int      dfn;               /**< Depth first search number. */
     int      uplink;            /**< dfn number of ancestor. */
-    bool     blacklisted;       /**< Marks whether the node is necessary in every induced scc */
+    int      depth;             /**< iteration depth of scc search */
 } scc_irn_info_t;
 
 
@@ -127,33 +110,100 @@ static ir_node *pop(scc_env_t *env)
     return n;
 }
 
+static ir_graph *build_graph(void)
+{
+    ir_type *type = new_type_method(0, 0);
+    ir_entity *ent = new_entity(get_glob_type(), "______test", type);
+    ir_graph *g = new_ir_graph(ent, 10);
 
-/** returns false if n must be ignored (either because it's not a Phi node or because it's been blacklisted */
-static bool find_scc_at(ir_node *n, scc_env_t *env)
+    set_current_ir_graph(g);
+
+    // ir_node *ifLeft = new_Block(1, )
+    // TODO: create test graph to work on
+
+
+    clear_irg_constraints(g, IR_GRAPH_CONSTRAINT_CONSTRUCTION);
+    return g;
+}
+
+/** returns whether another iteration is needed */
+static bool prepare_next_iteration(scc_env_t *env) {
+    // check if any scc is redundant
+    // for redundant sccs: add their nodes to the replacement_map
+    // for nonredundant sccs: build a new working set, dispose of the old ones
+
+    list_for_each_entry_safe(scc_node_t, scc, tmp, &env->sccs, link) {
+
+        ir_node *unique_pred = NULL;
+        bool redundant = true;
+        foreach_ir_nodeset(&scc->irns, irn, iter) {
+            // only nodes which are not on the "rim" of the scc are eligible for the next iteration
+            bool eligible_for_next_iteration = true;
+            foreach_irn_in(irn, idx, original_pred) {
+                // previous iterations might have "deleted" the node already.
+                ir_node *pred = ir_nodehashmap_get(ir_node, &env->replacement_map, original_pred);
+                if (pred == NULL) pred = original_pred;
+
+                if (!ir_nodeset_contains(&scc->irns, pred)) {
+                    if (unique_pred) redundant = false;
+                    // we don't break out of the loop because we still want to mark all necessary nodes
+                    unique_pred = pred;
+                    eligible_for_next_iteration = false;
+                }
+            }
+            if (eligible_for_next_iteration) {
+                scc_irn_info_t *info = get_irn_info(irn, env);
+                info->depth++;
+                info->dfn = 0;
+            } else {
+                ir_nodeset_remove_iterator(&scc->irns, &iter);
+            }
+        }
+        if (redundant) {
+            assert(unique_pred != NULL && "completely isolated phi cycles aren't supposed to exist!");
+            foreach_ir_nodeset(&scc->irns, irn, iter) {
+                ir_nodehashmap_insert(&env->replacement_map, irn, unique_pred);
+            }
+        }
+
+        if (ir_nodeset_size(&scc->irns) <= 1) {
+            // we have no need for this scc anymore (trivial phis are excluded by construction)
+            ir_nodeset_destroy(&scc->irns);
+            list_del_init(&scc->link);
+        }
+    }
+    printf("next iteration is a go!\n");
+    return !list_empty(&env->sccs);
+
+}
+
+/** returns false if n must be ignored
+ *  (either because it's not a Phi node or because it's been excluded in a previous run) */
+static bool find_scc_at(ir_node *n, scc_env_t *env, int depth)
 {
     scc_irn_info_t *info = get_irn_info(n, env);
 
-    if (info->blacklisted)
+    if (info->depth < depth)
         printf("blacklisted node: %d\n", get_irn_idx(n));
 
-    if (!is_Phi(n) || info->blacklisted) {
+    if (!is_Phi(n) || info->depth < depth) {
         return false;
-    } else {
-        printf("PHI: %d", get_irn_idx(n));
     }
     if (info->dfn != 0) {
         // node has already been visited
-        printf(" (x)\n");
         return true;
     }
-    printf("\n");
     info->dfn = ++env->next_index;
     info->uplink = info->dfn;
     push(env, n);
     info->in_stack = true;
     foreach_irn_in(n, i, pred) {
-        scc_irn_info_t *pred_info = get_irn_info(pred, env);
-        if (pred_info->dfn == 0 && find_scc_at(pred, env)) {
+        // the node might have been identified as part of a redundant scc already, so we need to check
+        ir_node *canonical_pred = ir_nodehashmap_get(ir_node, &env->replacement_map, pred);
+        if (!canonical_pred) canonical_pred = pred;
+
+        scc_irn_info_t *pred_info = get_irn_info(canonical_pred, env);
+        if (pred_info->dfn == 0 && find_scc_at(canonical_pred, env, depth)) {
             info->uplink = MIN(pred_info->uplink,info->uplink);
         } else if (pred_info->in_stack) {
             info->uplink = MIN(pred_info->dfn, info->uplink);
@@ -178,23 +228,6 @@ static bool find_scc_at(ir_node *n, scc_env_t *env)
     return true;
 }
 
-static bool is_redundant(scc_node_t *scc, scc_env_t *env)
-{
-    ir_node *unique_pred = NULL;
-    bool redundant = true;
-    foreach_ir_nodeset(&scc->irns, irn, iter) {
-        foreach_irn_in(irn, idx, pred) {
-            if (!ir_nodeset_contains(&scc->irns, pred)) {
-                if (unique_pred) redundant = false;
-                // we don't break out of the loop because we still want to mark all necessary nodes
-                unique_pred = pred;
-                get_irn_info(irn, env)->blacklisted = true;
-            }
-        }
-    }
-    return redundant;
-}
-
 static void print_sccs(scc_env_t *env)
 {
     if (!list_empty(&env->sccs)) {
@@ -203,16 +236,17 @@ static void print_sccs(scc_env_t *env)
             foreach_ir_nodeset(&scc->irns, irn, iter) {
                 printf("%d, ", get_irn_idx(irn));
             }
-            printf("]");
-            printf(is_redundant(scc, env) ? " (redundant)\n" : "\n");
+            printf("]\n");
         }
     } else {
         printf("No SCCs found :(\n");
     }
 }
 
-void opt_remove_unnecessary_phi_sccs(ir_graph *irg)
+FIRM_API void opt_remove_unnecessary_phi_sccs(ir_graph *irg)
 {
+    //build_graph();
+
     ir_add_dump_flags(ir_dump_flag_idx_label);
     dump_ir_graph(irg, "rofl");
 
@@ -222,6 +256,7 @@ void opt_remove_unnecessary_phi_sccs(ir_graph *irg)
     obstack_init(&temp);
     env.obst = temp;
     env.stack = NEW_ARR_F(ir_node*, 128);
+    ir_nodehashmap_init(&env.replacement_map);
     INIT_LIST_HEAD(&env.sccs);
 
     ir_reserve_resources(irg, IR_RESOURCE_IRN_LINK);
@@ -229,17 +264,30 @@ void opt_remove_unnecessary_phi_sccs(ir_graph *irg)
 
     printf("Starting phi SCC removal\n");
 
-    irg_walk_graph(irg, (void (*)(ir_node *,void *)) find_scc_at, NULL, &env);
+    void start(ir_node *irn, void *env) {
+        find_scc_at(irn, (scc_env_t *) env, 0);
+    }
+
+    irg_walk_graph(irg, start, NULL, &env);
     print_sccs(&env);
 
-    printf("======================================\n Second round\n======================================\n");
-    list_for_each_entry(scc_node_t, scc, &env.sccs, link) {
-        foreach_ir_nodeset(&scc->irns, irn, iter) {
-            find_scc_at(irn, &env);
+
+    while (prepare_next_iteration(&env)) {
+        list_head working_set = env.sccs;
+        INIT_LIST_HEAD(&env.sccs);
+        printf("======================================\n next round\n======================================\n");
+        list_for_each_entry(scc_node_t, scc, &working_set, link) {
+            foreach_ir_nodeset(&scc->irns, irn, iter) {
+                find_scc_at(irn, &env, 1);
+            }
+            printf("-------- NEXT SCC\n");
         }
-        printf("-------- NEXT SCC\n");
+        print_sccs(&env);
+        // dispose of the old working set
+        list_for_each_entry(scc_node_t, scc, &working_set, link) {
+            ir_nodeset_destroy(&scc->irns);
+        }
     }
-    print_sccs(&env);
 
     printf("Done.\n");
 
